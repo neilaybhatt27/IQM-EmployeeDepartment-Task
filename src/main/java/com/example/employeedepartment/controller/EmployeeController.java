732031d1fc65602fc6fa9a2d43dc4788db50c015
Neilay Bhatt <neilaybhatt@53s-MacBook-Air.local>
2024-01-08 18:25:05 +0530

    Added optional and default fields for the pagination and sorting of getAllEmployees()

--- src/main/java/com/example/employeedepartment/controller/EmployeeController.java
+++ src/main/java/com/example/employeedepartment/controller/EmployeeController.java
@@ -31,25 +31,38 @@ public class EmployeeController {
     /**
      * This GET API request gets all the employees and their details and sends the response to the client.
      * It has pagination which allows the API to fetch only 5 entries per page to reduce load.
+     * It also has sorting by name and sorting by id feature. (Optional)
+     * It also has search term available so that you can get details according to the search term. (Optional)
+     *
+     * @param page          (Optional) Page input for the pagination. Default value is 0
+     * @param size          (Optional) Size input for the pagination. Refers the size of data to be fetched per page. Default value is 5.
+     * @param sortField     (Optional) parameter based on which sorting happens. Must be either name or id. Default value is id.
+     * @param sortDirection (Optional) decides whether the sorting would be ascending or descending. Default value is asc.
+     * @param searchTerm    (Optional) Fetches data according to the input. It would happen either by name or id.
      * @return ArrayList containing all the employees and their details.
      */
     @GetMapping(path = "/all")
-    public @ResponseBody List<Employee> getAllEmployees(@RequestParam("page") int page, @RequestParam("size") int size, @RequestParam("sortField") String sortField, @RequestParam("sortDirection") String sortDirection, @RequestParam("searchTerm") String searchTerm) {
+    public @ResponseBody List<Employee> getAllEmployees(@RequestParam(value = "page", required = false, defaultValue = "0") int page, @RequestParam(value = "size", required = false, defaultValue = "5") int size, @RequestParam(value = "sortField", required = false, defaultValue = "id") String sortField, @RequestParam(value = "sortDirection", required = false, defaultValue = "asc") String sortDirection, @RequestParam(value = "searchTerm", required = false) String searchTerm) {
+        if (page < 0 || size <= 0 || !sortField.equals("id") && !sortField.equals("name") || !sortDirection.equals("asc") && !sortDirection.equals("desc") && !sortDirection.equals("ASC") && !sortDirection.equals("DESC")) {
+            throw new IllegalArgumentException("Invalid parameters");
+        }
         return employeeService.getAllEmployees(page, size, sortField, sortDirection, searchTerm);
     }
 
     /**
      * This GET API request gets the details of the employee whose id has been passed as the parameter.
+     *
      * @param id id of the requested employee.
      * @return Employee object containing the necessary details.
      */
-    @GetMapping(path="/{id}")
+    @GetMapping(path = "/{id}")
     public @ResponseBody Employee getEmployeeById(@PathVariable Long id) {
         return employeeService.getEmployeeById(id);
     }
 
     /**
      * This POST API request adds a new Employee to the system.
+     *
      * @param newEmployee This request body contains details of the new Employee. It must have name, role and departmentId.
      * @return A success message string.
      */
@@ -61,7 +74,8 @@ public class EmployeeController {
 
     /**
      * This PUT API request updates the employee details according to the input from the client.
-     * @param id the id which is passed as a parameter in the API endpoint. Refers to the id in the Employee table in the database.
+     *
+     * @param id              the id which is passed as a parameter in the API endpoint. Refers to the id in the Employee table in the database.
      * @param updatedEmployee Contains the updates required from the request body. Needs to have name, role and departmentId in the request.
      * @return A success message String
      */
@@ -73,6 +87,7 @@ public class EmployeeController {
 
     /**
      * This API request to delete the specified employee from the database.
+     *
      * @param id - refers to the id in the Employee table in the database. Parameter in the API endpoint.
      * @return A success message string
      */
--- src/main/java/com/example/employeedepartment/dao/EmployeeDao.java
+++ src/main/java/com/example/employeedepartment/dao/EmployeeDao.java
@@ -17,7 +17,7 @@ import com.example.employeedepartment.model.Employee;
 public class EmployeeDao {
     private final DataSource dataSource;
 
-    public EmployeeDao(DataSource dataSource){
+    public EmployeeDao(DataSource dataSource) {
         this.dataSource = dataSource;
     }
 
@@ -27,33 +27,54 @@ public class EmployeeDao {
      * @param employee - Employee object received from client side
      */
     public void save(Employee employee) {
-        try(Connection conn = dataSource.getConnection()) {
+        try (Connection conn = dataSource.getConnection()) {
             PreparedStatement pstmt = conn.prepareStatement("INSERT INTO employee (name, role, department_id) VALUES (?, ?, ?)");
             pstmt.setString(1, employee.getName());
             pstmt.setString(2, employee.getRole());
             pstmt.setLong(3, employee.getDepartmentId());
             pstmt.executeUpdate();
-        } catch (SQLException ex){
+        } catch (SQLException ex) {
             throw new RuntimeException();
         }
     }
 
     /**
      * This function returns all the employees with pagination to reduce load.
+     * Also contains sorting by name and id feature.
+     * Also contains filtering by search term.
+     *
+     * @param page          page number for pagination.
+     * @param size          number of rows to be sent for a single page.
+     * @param sortField     Sort by id or by name.
+     * @param sortDirection Ascending or descending sorting.
+     * @param searchTerm    String used for filtering by name or id.
      * @return List of all employees found in the database
      */
     public List<Employee> getAll(int page, int size, String sortField, String sortDirection, String searchTerm) {
         List<Employee> employees = new ArrayList<>();
+        String query = "SELECT * FROM employee";
+        int paramIndex = 0;
 
-        try(Connection conn = dataSource.getConnection()) {
-            PreparedStatement pstmt = conn.prepareStatement("SELECT * FROM employee WHERE id LIKE ? OR name LIKE ? ORDER BY " + sortField + " " + sortDirection + " " + "LIMIT ? OFFSET ?");
-            pstmt.setString(1, searchTerm);
-            pstmt.setString(2, "%" + searchTerm + "%");
-            pstmt.setInt(3, size);
-            pstmt.setInt(4, page * size);
+
+        if (searchTerm != null) {
+            query += " WHERE id = ? OR name LIKE ?";
+        }
+
+        query += " ORDER BY " + sortField + " " + sortDirection + " LIMIT ? OFFSET ?";
+
+        try (Connection conn = dataSource.getConnection()) {
+            PreparedStatement pstmt = conn.prepareStatement(query);
+
+            if (searchTerm != null) {
+                pstmt.setString(++paramIndex, searchTerm);
+                pstmt.setString(++paramIndex, searchTerm + "%");
+            }
+            pstmt.setInt(++paramIndex, size);
+            pstmt.setInt(++paramIndex, page * size);
+
             ResultSet resultSet = pstmt.executeQuery();
 
-            while (resultSet.next()){
+            while (resultSet.next()) {
                 Employee employee = new Employee();
                 employee.setId(resultSet.getLong("id"));
                 employee.setName(resultSet.getString("name"));
@@ -61,7 +82,7 @@ public class EmployeeDao {
                 employee.setDepartmentId(resultSet.getLong("department_id"));
                 employees.add(employee);
             }
-        } catch (SQLException ex){
+        } catch (SQLException ex) {
             throw new RuntimeException();
         }
 
@@ -70,18 +91,19 @@ public class EmployeeDao {
 
     /**
      * This function gets the specific employee by matching the id.
+     *
      * @param id - id of the requested employee
      * @return Employee object of the specified id.
      */
     public Employee getById(Long id) {
         Employee employee = new Employee();
 
-        try(Connection conn = dataSource.getConnection()) {
+        try (Connection conn = dataSource.getConnection()) {
             PreparedStatement pstmt = conn.prepareStatement("SELECT * FROM employee WHERE id = ?");
             pstmt.setLong(1, id);
             ResultSet resultSet = pstmt.executeQuery();
 
-            if(resultSet.next()){
+            if (resultSet.next()) {
                 employee.setId(resultSet.getLong("id"));
                 employee.setName(resultSet.getString("name"));
                 employee.setRole(resultSet.getString("role"));
@@ -96,11 +118,12 @@ public class EmployeeDao {
 
     /**
      * This function updates the employee details of the specified employee in the database
-     * @param id id of the employee that needs to be updated
+     *
+     * @param id       id of the employee that needs to be updated
      * @param employee Employee object with the updated details
      */
     public void update(long id, Employee employee) {
-        try(Connection conn = dataSource.getConnection()) {
+        try (Connection conn = dataSource.getConnection()) {
             PreparedStatement pstmt = conn.prepareStatement("UPDATE employee SET name = ?, role = ?, department_id = ? WHERE id = ?");
             pstmt.setString(1, employee.getName());
             pstmt.setString(2, employee.getRole());
@@ -114,10 +137,11 @@ public class EmployeeDao {
 
     /**
      * This function deletes the specified employee from the database
+     *
      * @param id - id of the employee that needs to be deleted
      */
     public void delete(long id) {
-        try(Connection conn = dataSource.getConnection()) {
+        try (Connection conn = dataSource.getConnection()) {
             PreparedStatement pstmt = conn.prepareStatement("DELETE FROM employee WHERE id = ?");
             pstmt.setLong(1, id);
             pstmt.executeUpdate();
