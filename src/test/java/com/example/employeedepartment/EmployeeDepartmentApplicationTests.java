00e1b14d7ced43e3ad7baf7acadcfd4743c26b42
Neilay Bhatt <neilaybhatt@53s-MacBook-Air.local>
2024-01-10 15:01:21 +0530

    Applied exception handling in the most basic way without using annotations. Applied in getAllEmployees() and getEmployeeById() endpoints.

--- src/test/java/com/example/employeedepartment/EmployeeDepartmentApplicationTests.java
+++
@@ -1,9 +0,0 @@
-package com.example.employeedepartment;
-
-
-import org.junit.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class EmployeeDepartmentApplicationTests {
-}
